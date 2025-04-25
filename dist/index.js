991db644797732b5b77da8c613bf99814f60eb1a
yaito3014 <ykakeyama3014@gmail.com>
2025-04-26 07:00:02 +0900

    update

--- dist/index.js
+++ dist/index.js
@@ -8453,19 +8453,16 @@ if (!process.env.GITHUB_REF?.startsWith("refs/pull/")) {
     console.log("not a pull request, exiting.");
     process.exit(0);
 }
-console.log(process.env.APP_ID);
-console.log(process.env.INPUT_APP_ID);
 const appId = parseInt(process.env.INPUT_APP_ID);
 const privateKey = process.env.INPUT_PRIVATE_KEY;
 const installationId = parseInt(process.env.INPUT_INSTALLATION_ID);
 const clientId = process.env.INPUT_CLIENT_ID;
 const clientSecret = process.env.INPUT_CLIENT_SECRET;
-console.log(appId, installationId, clientId);
 const app = new App({ appId, privateKey, oauth: { clientId, clientSecret } });
 const octokit = await app.getInstallationOctokit(installationId);
 const [owner, repo] = process.env.GITHUB_REPOSITORY?.split("/");
 const pull_request_number = parseInt(process.env.GITHUB_REF?.split("/")[2]);
-let body = "";
+let body = null;
 for (const file of readdirSync(".")) {
     if (!file.startsWith("compilation") || !file.endsWith(".log")) {
         continue;
@@ -8474,13 +8471,18 @@ for (const file of readdirSync(".")) {
     const regex = /warning( .\d+)?:/;
     const match_result = compilation_output.match(regex);
     if (match_result && match_result.length > 0) {
-        body += `detected warnings in the compilation output: <details><summary>compilation output</summary>\n\n\`\`\`\n${compilation_output}\n\`\`\`\n</details>\n`;
+        body =
+            body ||
+                body +
+                    `detected warnings in the compilation output: <details><summary>compilation output</summary>\n\n\`\`\`\n${compilation_output}\n\`\`\`\n</details>\n`;
     }
 }
-octokit.rest.issues.createComment({
-    owner,
-    repo,
-    issue_number: pull_request_number,
-    body,
-});
+if (body) {
+    octokit.rest.issues.createComment({
+        owner,
+        repo,
+        issue_number: pull_request_number,
+        body,
+    });
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ for (const file of readdirSync(".")) {
   }
 }
 
-if (body !== null) {
+if (body) {
   octokit.rest.issues.createComment({
     owner,
     repo,
