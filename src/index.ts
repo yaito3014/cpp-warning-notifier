c7dd36e9e0b35bfa32650c6d8adb904da5ff013f
yaito3014 <ykakeyama3014@gmail.com>
2025-04-26 02:10:34 +0900

    debug

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import { readFileSync } from "fs";
 
 import { parseString } from "gcc-output-parser";
 
+const compilation_output = readFileSync("compilation.log");
+const outputs = parseString(compilation_output);
+
+console.log("Parsed compilation output:", outputs);
+
 // if the action is triggered by not a pull request, exit
 if (!process.env.GITHUB_REF?.startsWith("refs/pull/")) {
   console.log("Not a pull request, exiting.");
@@ -13,9 +18,6 @@ const octokit = new Octokit();
 
 const [owner, repo] = process.env.GITHUB_REPOSITORY?.split("/")!;
 const pull_request_number = parseInt(process.env.GITHUB_REF?.split("/")[2]!);
-const compilation_output = readFileSync("compilation.log");
-
-const outputs = parseString(compilation_output);
 
 octokit.rest.issues.createComment({
   owner,
