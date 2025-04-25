956d73a5c1a345da276f2a37f94d0a15d6cf8fad
yaito3014 <ykakeyama3014@gmail.com>
2025-04-25 23:06:39 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# cpp-warning-notifier
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { Octokit } from "@octokit/action";
+
+const octokit = new Octokit();
+
+const [owner, repo] = process.env.GITHUB_REPOSITORY?.split("/")!;
+const pull_request_number = parseInt(process.env.GITHUB_REF?.split("/")[2]!);
+
+const { data: pullRequest } = await octokit.rest.pulls.get({
+  owner,
+  repo,
+  pull_number: pull_request_number,
+});
+
+const { data: actions } = await octokit.rest.actions.listWorkflowRunsForRepo({
+  owner,
+  repo,
+});
+
+const workflow_runs = actions.workflow_runs.filter((action) => {
+  return (
+    action.head_branch === pullRequest.head.ref &&
+    action.head_sha === pullRequest.head.sha &&
+    action.status === "completed"
+  );
+});
+
+const latest = workflow_runs.reduce((latest, action) => {
+  if (!latest) return action;
+  return new Date(action.created_at) > new Date(latest.created_at)
+    ? action
+    : latest;
+});
+
+octokit.rest.issues.createComment({
+  owner,
+  repo,
+  issue_number: pull_request_number,
+  body: `The latest workflow run for this pull request is [#${latest.run_number}](${latest.html_url})`,
+});
