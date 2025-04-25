bc16809de8da13719f860e62ff18b27305f18de2
yaito3014 <ykakeyama3014@gmail.com>
2025-04-26 03:49:36 +0900

    traverse

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,6 @@
 import { Octokit } from "@octokit/action";
-import { readFileSync } from "fs";
+import { readdirSync, readFileSync } from "fs";
 
-const compilation_output = readFileSync("compilation.log").toString();
-
-const regex = /warning( .\d+)?:/;
-
-const match_result = compilation_output.match(regex);
-
-if (match_result && match_result.length > 0) {
-  console.log("compilation output contains warnings.");
-}
-
-// if the action is triggered by not a pull request, exit
 if (!process.env.GITHUB_REF?.startsWith("refs/pull/")) {
   console.log("not a pull request, exiting.");
   process.exit(0);
@@ -22,9 +11,28 @@ const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY?.split("/")!;
 const pull_request_number = parseInt(process.env.GITHUB_REF?.split("/")[2]!);
 
+let body = "";
+
+for (const file of readdirSync(".")) {
+  if (!file.startsWith("compilation") || !file.endsWith(".log")) {
+    console.log(`skipping ${file}`);
+    continue;
+  }
+
+  const compilation_output = readFileSync(file).toString();
+
+  const regex = /warning( .\d+)?:/;
+
+  const match_result = compilation_output.match(regex);
+
+  if (match_result && match_result.length > 0) {
+    body += `detected warnings in the compilation output: <details><summary>compilation output</summary>\n\n\`\`\`\n${compilation_output}\n\`\`\`\n</details>\n`;
+  }
+}
+
 octokit.rest.issues.createComment({
   owner,
   repo,
   issue_number: pull_request_number,
-  body: `detected warnings in the compilation output: <details><summary>compilation output</summary>\n\n\`\`\`\n${compilation_output}\n\`\`\`\n</details>`,
+  body,
 });
