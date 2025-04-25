3497fc67b92764fe5c80937734b713f1c87b5267
KakeyamaY <ykakeyama3014@gmail.com>
2025-04-26 04:52:44 +0900

    add warning (#2)
    * add warning
    
    * debug
    
    * make object
    
    * refine type def
    
    * comment json
    
    * update
    
    * fix
    
    * update whitespace
    
    * don't include warnings itself
    
    * exit when no error or warning
    
    * eliminate gcc-output-parser
    
    * invoke
    
    * detect warning
    
    * fix
    
    * cmake
    
    * add pragma
    
    * add -e option
    
    * remove newline
    
    * traverse
    
    * test app
    
    * wrong context
    
    * add installation id
    
    * debug
    
    * specify environment
    
    * fix

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,46 @@
-import { Octokit } from "@octokit/action";
-import { readFileSync } from "fs";
+import { readdirSync, readFileSync } from "fs";
+import { App } from "octokit";
 
-import { parseString } from "gcc-output-parser";
-
-// if the action is triggered by not a pull request, exit
 if (!process.env.GITHUB_REF?.startsWith("refs/pull/")) {
-  console.log("Not a pull request, exiting.");
+  console.log("not a pull request, exiting.");
   process.exit(0);
 }
 
-const octokit = new Octokit();
+const appId = parseInt(process.env.APP_ID!);
+const privateKey = process.env.PRIVATE_KEY!;
+const installationId = parseInt(process.env.INSTALLATION_ID!);
+const clientId = process.env.CLIENT_ID!;
+const clientSecret = process.env.CLIENT_SECRET!;
+
+console.log(appId, installationId, clientId);
+
+const app = new App({ appId, privateKey, oauth: { clientId, clientSecret } });
+const octokit = await app.getInstallationOctokit(installationId);
 
 const [owner, repo] = process.env.GITHUB_REPOSITORY?.split("/")!;
 const pull_request_number = parseInt(process.env.GITHUB_REF?.split("/")[2]!);
-const compilation_output = readFileSync("compilation.log");
 
-const outputs = parseString(compilation_output);
+let body = "";
+
+for (const file of readdirSync(".")) {
+  if (!file.startsWith("compilation") || !file.endsWith(".log")) {
+    continue;
+  }
+
+  const compilation_output = readFileSync(file).toString();
+
+  const regex = /warning( .\d+)?:/;
+
+  const match_result = compilation_output.match(regex);
+
+  if (match_result && match_result.length > 0) {
+    body += `detected warnings in the compilation output: <details><summary>compilation output</summary>\n\n\`\`\`\n${compilation_output}\n\`\`\`\n</details>\n`;
+  }
+}
 
 octokit.rest.issues.createComment({
   owner,
   repo,
   issue_number: pull_request_number,
-  body: `compilation output is:\n\n\`\`\`\n${outputs}\n\`\`\``,
+  body,
 });
