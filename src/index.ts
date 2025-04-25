b505bff72ac89f08a4f32e7312d34d905a6a0289
yaito3014 <ykakeyama3014@gmail.com>
2025-04-25 23:38:24 +0900

    handle empty case

--- src/index.ts
+++ src/index.ts
@@ -23,17 +23,20 @@ const workflow_runs = actions.workflow_runs.filter((action) => {
     action.status === "completed"
   );
 });
+if (workflow_runs.length === 0) {
+  console.log("No workflow runs found for this pull request.");
+} else {
+  const latest = workflow_runs.reduce((latest, action) => {
+    if (!latest) return action;
+    return new Date(action.created_at) > new Date(latest.created_at)
+      ? action
+      : latest;
+  });
 
-const latest = workflow_runs.reduce((latest, action) => {
-  if (!latest) return action;
-  return new Date(action.created_at) > new Date(latest.created_at)
-    ? action
-    : latest;
-});
-
-octokit.rest.issues.createComment({
-  owner,
-  repo,
-  issue_number: pull_request_number,
-  body: `The latest workflow run for this pull request is [#${latest.run_number}](${latest.html_url})`,
-});
+  octokit.rest.issues.createComment({
+    owner,
+    repo,
+    issue_number: pull_request_number,
+    body: `The latest workflow run for this pull request is [#${latest.run_number}](${latest.html_url})`,
+  });
+}
