f357b0b866f746907f4b8ed215cd24a75f08e98c
yaito3014 <ykakeyama3014@gmail.com>
2025-04-26 01:37:08 +0900

    remove unneeded code

--- src/index.ts
+++ src/index.ts
@@ -6,36 +6,10 @@ const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY?.split("/")!;
 const pull_request_number = parseInt(process.env.GITHUB_REF?.split("/")[2]!);
 const compilation_output = readFileSync("compilation.log");
-const { data: pullRequest } = await octokit.rest.pulls.get({
+
+octokit.rest.issues.createComment({
   owner,
   repo,
-  pull_number: pull_request_number,
+  issue_number: pull_request_number,
+  body: `compilation output is:\n\n\`\`\`\n${compilation_output}\n\`\`\``,
 });
-
-const { data: actions } = await octokit.rest.actions.listWorkflowRunsForRepo({
-  owner,
-  repo,
-});
-
-const workflow_runs = actions.workflow_runs.filter(
-  (action) =>
-    action.head_branch === pullRequest.head.ref && action.status === "completed"
-);
-
-if (workflow_runs.length === 0) {
-  console.log("No workflow runs found for this pull request.");
-} else {
-  const latest = workflow_runs.reduce((latest, action) => {
-    if (!latest) return action;
-    return new Date(action.created_at) > new Date(latest.created_at)
-      ? action
-      : latest;
-  });
-
-  octokit.rest.issues.createComment({
-    owner,
-    repo,
-    issue_number: pull_request_number,
-    body: `The latest workflow run for this pull request is [#${latest.run_number}](${latest.html_url}), and the compilation output is:\n\n\`\`\`\n${compilation_output}\n\`\`\``,
-  });
-}
