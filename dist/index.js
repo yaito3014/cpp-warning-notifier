8f46711a7d2203da77efceee7d03e3a582139325
yaito3014 <ykakeyama3014@gmail.com>
2025-04-26 11:58:44 +0900

    use numbered list

--- dist/index.js
+++ dist/index.js
@@ -8492,7 +8492,7 @@ for (const file of readdirRecursively(".")) {
     const outputMatch = compilationOutput.match(/warning( .\d+)?:/);
     if (outputMatch && outputMatch.length > 0) {
         const url = `https://github.com/${owner}/${repo}/actions/runs/${runId}/job/${jobId}#step:${stepId}:1`;
-        const appendString = `detected warnings in the compilation output\n<${url}>\n\n`;
+        const appendString = `1. <${url}>\n`;
         if (body) {
             body += appendString;
         }
--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ for (const file of readdirRecursively(".")) {
   if (outputMatch && outputMatch.length > 0) {
     const url = `https://github.com/${owner}/${repo}/actions/runs/${runId}/job/${jobId}#step:${stepId}:1`;
 
-    const appendString = `detected warnings in the compilation output\n<${url}>\n\n`;
+    const appendString = `1. <${url}>\n`;
     if (body) {
       body += appendString;
     } else {
