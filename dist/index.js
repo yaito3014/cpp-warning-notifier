2812d4e81b6c5a1d69a5c88fedf1d28e777b4b48
yaito3014 <ykakeyama3014@gmail.com>
2025-04-28 21:52:44 +0900

    build

--- dist/index.js
+++ dist/index.js
@@ -8540,7 +8540,6 @@ console.log(matrix);
 const renderHTML = (mat) => {
     let body = "";
     let count = 0;
-    body += `<tr>`;
     for (const [key, val] of Object.entries(mat)) {
         if (Array.isArray(val)) {
             ++count;
@@ -8548,6 +8547,7 @@ const renderHTML = (mat) => {
             for (const elem of val) {
                 body += `<td>${elem}</td>\n`;
             }
+            body = `<tr>${body}</tr>`;
         }
         else {
             const { count: innerCount, body: innerBody } = renderHTML(val);
@@ -8555,7 +8555,6 @@ const renderHTML = (mat) => {
             body += innerBody;
         }
     }
-    body += `</tr>`;
     return { count, body };
 };
 console.log("body is", body);
